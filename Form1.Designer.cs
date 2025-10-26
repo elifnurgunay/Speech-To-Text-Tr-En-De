c1fba82cd24b0a1e0eaaa10045327dd37b67a0ba
elif4444 <elifnurgunay06@gmail.com>
2025-10-26 22:01:17 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Speech_To_Text
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.comboBoxDilSecimi = new System.Windows.Forms.ComboBox();
+            this.butonBaslatDurdur = new System.Windows.Forms.Button();
+            this.textBoxMetinCikisi = new System.Windows.Forms.TextBox();
+            this.labelKismiSonuc = new System.Windows.Forms.Label();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.butonTemizle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // comboBoxDilSecimi
+            this.comboBoxDilSecimi.FormattingEnabled = true;
+            this.comboBoxDilSecimi.Location = new System.Drawing.Point(120, 20);
+            this.comboBoxDilSecimi.Name = "comboBoxDilSecimi";
+            this.comboBoxDilSecimi.Size = new System.Drawing.Size(200, 28);
+            this.comboBoxDilSecimi.TabIndex = 0;
+
+            // butonBaslatDurdur
+            this.butonBaslatDurdur.Location = new System.Drawing.Point(340, 20);
+            this.butonBaslatDurdur.Name = "butonBaslatDurdur";
+            this.butonBaslatDurdur.Size = new System.Drawing.Size(100, 30);
+            this.butonBaslatDurdur.TabIndex = 1;
+            this.butonBaslatDurdur.Text = "Başlat";
+            this.butonBaslatDurdur.UseVisualStyleBackColor = true;
+            this.butonBaslatDurdur.Click += new System.EventHandler(this.butonBaslatDurdur_Click);
+
+            // textBoxMetinCikisi
+            this.textBoxMetinCikisi.Location = new System.Drawing.Point(20, 100);
+            this.textBoxMetinCikisi.Multiline = true;
+            this.textBoxMetinCikisi.Name = "textBoxMetinCikisi";
+            this.textBoxMetinCikisi.Size = new System.Drawing.Size(420, 150);
+            this.textBoxMetinCikisi.TabIndex = 2;
+
+            // labelKismiSonuc
+            this.labelKismiSonuc.AutoSize = true;
+            this.labelKismiSonuc.Location = new System.Drawing.Point(20, 260);
+            this.labelKismiSonuc.Name = "labelKismiSonuc";
+            this.labelKismiSonuc.Size = new System.Drawing.Size(75, 20);
+            this.labelKismiSonuc.TabIndex = 3;
+            this.labelKismiSonuc.Text = "Kısmi: ";
+
+            // listBoxLog
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.ItemHeight = 20;
+            this.listBoxLog.Location = new System.Drawing.Point(20, 300);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(420, 144);
+            this.listBoxLog.TabIndex = 4;
+
+            // butonTemizle
+            this.butonTemizle.Location = new System.Drawing.Point(340, 60);
+            this.butonTemizle.Name = "butonTemizle";
+            this.butonTemizle.Size = new System.Drawing.Size(100, 30);
+            this.butonTemizle.TabIndex = 5;
+            this.butonTemizle.Text = "Temizle";
+            this.butonTemizle.UseVisualStyleBackColor = true;
+            this.butonTemizle.Click += new System.EventHandler(this.butonTemizle_Click);
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Dil Seçimi:";
+
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tanınan Metin:";
+
+            // label3
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Log:";
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 473);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.butonTemizle);
+            this.Controls.Add(this.listBoxLog);
+            this.Controls.Add(this.labelKismiSonuc);
+            this.Controls.Add(this.textBoxMetinCikisi);
+            this.Controls.Add(this.butonBaslatDurdur);
+            this.Controls.Add(this.comboBoxDilSecimi);
+            this.Name = "Form1";
+            this.Text = "Çok Dilli Speech-to-Text";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ComboBox comboBoxDilSecimi;
+        private System.Windows.Forms.Button butonBaslatDurdur;
+        private System.Windows.Forms.TextBox textBoxMetinCikisi;
+        private System.Windows.Forms.Label labelKismiSonuc;
+        private System.Windows.Forms.ListBox listBoxLog;
+        private System.Windows.Forms.Button butonTemizle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using NAudio.Wave;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vosk;
+
+namespace Speech_To_Text
+{
+    public partial class Form1 : Form
+    {
+
+        private VoskRecognizer recognizer;
+        private WaveInEvent waveIn;
+        private Model model;
+        private bool kayitAktif = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ComboBoxDilSecimiDoldur();
+        }
+
+        private void ComboBoxDilSecimiDoldur()
+        {
+            // ComboBox'a dilleri ekle
+            comboBoxDilSecimi.Items.Add("Türkçe");
+            comboBoxDilSecimi.Items.Add("İngilizce");
+            comboBoxDilSecimi.Items.Add("Almanca");
+            comboBoxDilSecimi.SelectedIndex = 0; // Varsayılan olarak Türkçe
+        }
+
+        private async Task<bool> DilModeliYukleAsync(string dil)
+        {
+            string modelYolu = "";
+
+            // Model yollarını burada belirleyin.
+            // İsterseniz buraya mutlak yolu yazın veya çalışma dizinine göre relative bırakın.
+            switch (dil)
+            {
+                case "Türkçe":
+                    modelYolu = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "vosk-model-small-tr-0.3");
+                    break;
+                case "İngilizce":
+                    modelYolu = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "vosk-model-small-en-us-0.15");
+                    break;
+                case "Almanca":
+                    // Orijinalinizdeki boşluk/dash sorunlarını düzelttim; kendi klasör adınıza göre güncelleyin.
+                    modelYolu = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "vosk-model-small-de-0.15");
+                    break;
+                default:
+                    MessageBox.Show("Bilinmeyen dil seçimi.");
+                    return false;
+            }
+
+            if (!Directory.Exists(modelYolu))
+            {
+                MessageBox.Show($"{dil} modeli bulunamadı: {modelYolu}");
+                return false;
+            }
+
+            // Disable controls while loading
+            this.Invoke(new Action(() =>
+            {
+                butonBaslatDurdur.Enabled = false;
+                comboBoxDilSecimi.Enabled = false;
+                listBoxLog.Items.Add($"{dil} modeli yükleniyor: {modelYolu}");
+            }));
+
+            // Dispose old recognizer/model safely
+            try
+            {
+                if (recognizer != null)
+                {
+                    recognizer.Dispose();
+                    recognizer = null;
+                }
+
+                if (model != null)
+                {
+                    model.Dispose();
+                    model = null;
+                }
+            }
+            catch { }
+
+            try
+            {
+                // Load model in background thread to avoid UI freeze
+                await Task.Run(() =>
+                {
+                    model = new Model(modelYolu);
+                    recognizer = new VoskRecognizer(model, 16000.0f);
+                    recognizer.SetMaxAlternatives(0);
+                    recognizer.SetWords(true);
+                });
+
+                this.Invoke(new Action(() =>
+                {
+                    listBoxLog.Items.Add($"{dil} modeli yüklendi: {modelYolu}");
+                    MessageBox.Show("Model başarıyla yüklendi.");
+                }));
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    MessageBox.Show($"Model yüklenirken hata oluştu: {ex.Message}");
+                    listBoxLog.Items.Add($"Model yüklenirken hata: {ex.Message}");
+                }));
+
+                return false;
+            }
+            finally
+            {
+                // Re-enable controls
+                this.Invoke(new Action(() =>
+                {
+                    butonBaslatDurdur.Enabled = true;
+                    comboBoxDilSecimi.Enabled = true;
+                }));
+            }
+        }
+
+        private void SesKaydiniBaslat()
+        {
+            try
+            {
+                waveIn = new WaveInEvent();
+                waveIn.BufferMilliseconds = 50;
+                waveIn.WaveFormat = new WaveFormat(16000, 1); // 16kHz, mono
+                waveIn.DataAvailable += WaveIn_DataAvailable;
+                waveIn.RecordingStopped += WaveIn_RecordingStopped;
+
+                waveIn.StartRecording();
+                kayitAktif = true;
+
+                butonBaslatDurdur.Text = "Durdur";
+                listBoxLog.Items.Add("Ses kaydı başlatıldı");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ses kaydı başlatılamadı: {ex.Message}");
+            }
+        }
+
+
+        private void SesKaydiniDurdur()
+        {
+            if (waveIn != null)
+            {
+                try
+                {
+                    waveIn.DataAvailable -= WaveIn_DataAvailable;
+                    waveIn.RecordingStopped -= WaveIn_RecordingStopped;
+                    waveIn.StopRecording();
+                    waveIn.Dispose();
+                    waveIn = null;
+                }
+                catch { }
+
+                kayitAktif = false;
+                butonBaslatDurdur.Text = "Başlat";
+                listBoxLog.Items.Add("Ses kaydı durduruldu");
+            }
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (recognizer != null && e.BytesRecorded > 0)
+            {
+                // Ses verisini işle
+                if (recognizer.AcceptWaveform(e.Buffer, e.BytesRecorded))
+                {
+                    string sonucJson = recognizer.Result();
+                    var sonuc = JsonConvert.DeserializeObject<SpeechResult>(sonucJson);
+
+                    if (!string.IsNullOrEmpty(sonuc?.text))
+                    {
+                        // UI thread'ine güvenli şekilde erişim
+                        this.Invoke(new Action(() =>
+                        {
+                            textBoxMetinCikisi.Text = sonuc.text;
+                            listBoxLog.Items.Add($"Tanındı: {sonuc.text}");
+                        }));
+                    }
+                }
+                else
+                {
+                    // Kısmi sonuç
+                    string kismiSonucJson = recognizer.PartialResult();
+                    var kismiSonuc = JsonConvert.DeserializeObject<PartialSpeechResult>(kismiSonucJson);
+
+                    if (!string.IsNullOrEmpty(kismiSonuc?.partial))
+                    {
+                        this.Invoke(new Action(() =>
+                        {
+                            labelKismiSonuc.Text = $"Kısmi: {kismiSonuc.partial}";
+                        }));
+                    }
+                }
+            }
+        }
+
+        private void WaveIn_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (e.Exception != null)
+            {
+                MessageBox.Show($"Ses kaydı hatası: {e.Exception.Message}");
+            }
+        }
+
+        private async void butonBaslatDurdur_Click(object sender, EventArgs e)
+        {
+            if (!kayitAktif)
+            {
+                string secilenDil = comboBoxDilSecimi.SelectedItem?.ToString() ?? "Türkçe";
+                bool yuklemeBasarili = await DilModeliYukleAsync(secilenDil);
+                if (yuklemeBasarili)
+                {
+                    SesKaydiniBaslat();
+                }
+            }
+            else
+            {
+                SesKaydiniDurdur();
+            }
+        }
+
+        private async void comboBoxDilSecimi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (kayitAktif)
+            {
+                string secilenDil = comboBoxDilSecimi.SelectedItem?.ToString() ?? "Türkçe";
+
+                // Stop recording first to free audio device and recognizer
+                SesKaydiniDurdur();
+
+                bool yuklemeBasarili = await DilModeliYukleAsync(secilenDil);
+                if (yuklemeBasarili)
+                {
+                    SesKaydiniBaslat();
+                    listBoxLog.Items.Add($"Dil değiştirildi: {secilenDil}");
+                }
+            }
+        }
+
+        private void butonTemizle_Click(object sender, EventArgs e)
+        {
+            textBoxMetinCikisi.Clear();
+            labelKismiSonuc.Text = "Kısmi: ";
+            listBoxLog.Items.Clear();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Kaynakları temizle
+            SesKaydiniDurdur();
+
+            if (recognizer != null)
+                recognizer.Dispose();
+
+            if (model != null)
+                model.Dispose();
+
+            base.OnFormClosing(e);
+        }
+    }
+
+    // JSON deserialization için sınıflar
+    public class SpeechResult
+    {
+        public string text { get; set; }
+    }
+
+    public class PartialSpeechResult
+    {
+        public string partial { get; set; }
+    }
+}
